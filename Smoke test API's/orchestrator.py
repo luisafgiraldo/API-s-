fd1c76784eb8f67739880b7a81d94615e581375e
luisafg233 <luisafernandaaristizabalgirald@gmail.com>
2024-07-24 12:46:57 -0500

    add tests

---
+++ Smoke test API's/orchestrator.py
@@ -0,0 +1,129 @@
+####################### PARAMETERS ####################################
+import os
+import sys
+sys.path.append(r'C:\Users\user\Documents\Python Scripts\U\src\models')
+projectType = {
+    1 : 'classification',
+    2 : 'segmentation',
+    3 : 'object-detection'
+                }
+################### PARAMETERS TO CHANGE ###########################
+api_key = "land_sk_929mgCm9o1Hl59bHo1nLK0O9KH2wEcl4zDYUpYprJffl6f9onm"
+url_base = 'https://api.staging.landing.ai/v1'
+#%%
+################### PARAMETERS TO CHANGE ###########################
+elegir_proyecto = projectType.get(3) 
+name_proyecto = 'Jun15-OD'
+#%%
+######################## CREATE PROJECT ###############################
+import create_project as cp
+url = f"{url_base}/projects"
+data = {"projectType": elegir_proyecto, "name": name_proyecto}
+project_id = cp.create(api_key=api_key,url=url,data=data)
+#%% 
+######################## GET PROJECT ##################################
+import getProject as gp
+gp.getProject(api_key, url, project_id)
+#%%
+######################## CREATE CLASSES ###############################
+import create_classes as cc
+url = f'{url}/{project_id}/classes'
+if elegir_proyecto == 'classification':
+    data = {"0": {"name": "Roll Print"},"1": {"name": "Side line"}}
+elif elegir_proyecto ==  'segmentation':
+    data = {"1": {"name": "Screw"}}
+elif elegir_proyecto == 'object-detection':
+    data = {"1": {"name": "Screw", "color": "#FFFF00"}}
+cc.create(api_key=api_key, url=url, data=data)
+#%%
+####################### UPLOAD IMAGES #################################
+import upload_images as ui
+url = url.replace('classes', 'images')
+if elegir_proyecto == 'classification':
+    folders = [data[key]["name"] for key in data] # Se crea una lista, porque la clase es la misma carpeta
+    path = r'C:\Users\user\Desktop\images'
+    ui.upload_classification(api_key, url, folders, path)
+elif elegir_proyecto == 'object-detection':
+    path = r'C:\Users\user\Desktop\images\Snapshot-06-05-202422_44-171764549378\NoSplit'
+    ui.upload_object_detection(api_key, url, directory=path)
+    import auto_split as asp
+    url = url.replace('images', 'autosplit')
+    # Prepare the payload (adheres to the required format)
+    data = {
+        "splitPercentages": {
+            "train": 65,
+            "dev": 25,
+            "test": 10,
+        },
+        "selectOption": "all-labeled"
+    }
+    
+    asp.auto_split(url, api_key, data)
+    
+elif elegir_proyecto == 'segmentation':
+    path = r'C:\Users\user\Downloads\Loops & Screws - Labeled Simple - SEG - ZIP\Loops & Screws - Labeled Simple - SEG - ZIP\Labeled Simple - SEG\train'
+    ui.upload_segmentation(api_key, url, directory=path)
+    import auto_split as asp
+    url = url.replace('images', 'autosplit')
+    # Prepare the payload (adheres to the required format)
+    data = {
+        "splitPercentages": {
+            "train": 65,
+            "dev": 25,
+            "test": 10,
+        },
+        "selectOption": "all-labeled"
+    }
+    
+    asp.auto_split(url, api_key, data)
+#%%
+####################### TRAIN #########################################
+import train as t
+if elegir_proyecto == 'classification':
+    url = url.replace('images', 'train')
+elif elegir_proyecto == 'object-detection':
+    url = url.replace('autosplit', 'train')
+elif elegir_proyecto == 'segmentation':
+    url = url.replace('autosplit', 'train')
+training_id = t.training(api_key, url)
+###################### MONITOR TRAIN ##################################
+import monitor_train as mt
+url = f'{url}/{training_id}/status'
+mt.monitoring(api_key, url)
+#%%
+##################### MODELS #########################################
+import get_models as gm
+url = f'{url_base}/projects/{project_id}/models'
+models = gm.model(api_key, url)
+model = models[0]
+threshold = 0.5
+#%%
+###################### DEPLOYMENT #####################################
+import create_deployment as cd
+url = url.replace('models', 'deployments')
+if elegir_proyecto == 'classification':
+    data = {"name": model.get('name'), "modelId" : model.get('id'), "threshold": threshold}
+elif elegir_proyecto == 'object-detection':
+    data = {"name": model.get('name'), "modelId" : model.get('id')}
+elif elegir_proyecto == 'segmentation':
+    data = {"name": model.get('name'), "modelId" : model.get('id')}  
+deployment = cd.create(api_key, url, data)
+deployment = deployment['data']
+predictionUrl = deployment['predictionUrl']
+endpoint_id = deployment['id']
+#%%
+###################### PREDICT ########################################
+import predict as p
+url = url.replace('deployments', 'usage/summary')
+# result = p.get_data_from_api(url, api_key)
+# print(result)
+if elegir_proyecto ==  'classification':
+    folder = 'Predictions\Class' # back slash is placed because of path sharing
+elif elegir_proyecto  == 'object-detection':
+    folder = 'Images'
+elif elegir_proyecto  == 'segmentation':
+    folder = 'Images'
+image_dir = os.path.join(path, folder)
+p.predict(image_dir= image_dir,endpoint_id = endpoint_id,api_key = api_key)
+
+
