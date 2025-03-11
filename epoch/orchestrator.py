d695c5fc32c90c1b53dfb57c7ee4de336f58f9d6
VictorM10M <victormanueldiezmunoz@gmail.com>
2025-03-11 11:17:49 -0500

    Create cases to epoch trains

---
+++ epoch/orchestrator.py
@@ -0,0 +1,206 @@
+import custom_trainning as ct
+import pandas as pd
+import openpyxl
+from datetime import datetime
+import os
+
+# Base URL for API requests
+url_base = "https://api.staging.landing.ai/v1/projects"
+# API key for authentication
+api_key = "land_sk_0m6ToQoQz3jYgf9VZMQHBl3tJRaxYy02qlp3IC7q5UsXPnqctR"  # Org: Enterprise-Org
+
+# Dictionary to map project type IDs to their corresponding names
+projectType = {1: "classification", 2: "segmentation", 3: "object-detection"}
+
+# Dictionary to map training type IDs to their corresponding names
+trainType = {0: "Epoch"}
+
+# Create an empty DataFrame with specified columns
+df = pd.DataFrame(
+    columns=[
+        "type_train",
+        "type_project",
+        "project_id",
+        "time_training",
+        "architecture",
+        "rescaleWithPadding",
+        "train",
+        "dev",
+        "test",
+        "date",
+    ]
+)
+
+# List of commands for large images training
+v_commands_epoch_train = [
+    f"""
+    project_id=139863102765096,
+    name=None,
+    architecture="RtmDet-[9M]",
+    epochs=50,
+    height=580,
+    width=580,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=None,
+    magnitude=None,
+    p=None,
+    type_project="{projectType.get(3)}",
+    type_train="{trainType.get(0)}"
+
+     """,
+     f"""
+    project_id=139863102765096,
+    name=None,
+    architecture="RepPoints-[20M]",
+    epochs=100,
+    height=580,
+    width=580,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=None,
+    magnitude=None,
+    p=None,
+    type_project="{projectType.get(3)}",
+    type_train="{trainType.get(0)}"
+
+     """,
+     f"""
+    project_id=139863102765096,
+    name=None,
+    architecture="RepPoints-[37M]",
+    epochs=150,
+    height=580,
+    width=580,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=None,
+    magnitude=None,
+    p=None,
+    type_project="{projectType.get(3)}",
+    type_train="{trainType.get(0)}"
+
+     """,
+    f"""
+    project_id=139863342313515,
+    name=None,
+    architecture="SegFormer-[14M]",
+    epochs=50,
+    height=800,
+    width=800,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=1,
+    magnitude=4,
+    p=1,
+    type_project="{projectType.get(2)}",
+    type_train="{trainType.get(0)}"
+    """,
+    f"""
+    project_id=139863342313515,
+    name=None,
+    architecture="FastVit-[14M]",
+    epochs=100,
+    height=800,
+    width=800,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=1,
+    magnitude=4,
+    p=1,
+    type_project="{projectType.get(2)}",
+    type_train="{trainType.get(0)}"
+    """,
+    f"""
+    project_id=139863342313515,
+    name=None,
+    architecture="SegFormer-[14M]",
+    epochs=150,
+    height=800,
+    width=800,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=1,
+    magnitude=4,
+    p=1,
+    type_project="{projectType.get(2)}",
+    type_train="{trainType.get(0)}"
+    """,
+    f"""
+    project_id=139863258720298,
+    name=None,
+    architecture="ConvNext-[29M]",
+    epochs=50,
+    height=512,
+    width=512,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=None,
+    magnitude=None,
+    p=None,
+    type_project="{projectType.get(1)}",
+    type_train="{trainType.get(0)}"
+     """,
+     f"""
+    project_id=139863258720298,
+    name=None,
+    architecture="ConvNext-[16M]",
+    epochs=100,
+    height=512,
+    width=512,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=None,
+    magnitude=None,
+    p=None,
+    type_project="{projectType.get(1)}",
+    type_train="{trainType.get(0)}"
+     """,
+     f"""
+    project_id=139863258720298,
+    name=None,
+    architecture="ConvNext-[29M]",
+    epochs=150,
+    height=512,
+    width=512,
+    paddingValue=0,
+    horizontalFlip=0.5,
+    verticalFlip=None,
+    numberTransforms=None,
+    magnitude=None,
+    p=None,
+    type_project="{projectType.get(1)}",
+    type_train="{trainType.get(0)}"
+     """,
+]
+
+# Execute large images training commands and update DataFrame
+epoch_train = ct.execute(v_commands_epoch_train, url_base, api_key, df)
+
+result_vertical_reset = epoch_train
+
+# Define the file path
+file_path = os.path.join("epoch", "reports", "report.xlsx")
+
+# Check if the file exists
+if os.path.exists(file_path):
+    # If it exists, load the existing Excel file
+    existing_data = pd.read_excel(file_path)
+    # Concatenate the existing data with the new data
+    final_data = pd.concat([existing_data, result_vertical_reset], axis=0).reset_index(
+        drop=True
+    )
+else:
+    # If it does not exist, use the new data directly
+    final_data = result_vertical_reset
+
+# Save the final DataFrame to the Excel file
+final_data.to_excel(file_path, index=False)
