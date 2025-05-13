0dd72c3dd69ec7d462710392acea528e73235a1d
Tu Nombre <tuemail@ejemplo.com>
2025-05-13 09:25:21 -0500

    update rate limit

--- runner.js
+++ runner.js
@@ -10,6 +10,15 @@ try {
 
   const output3 = execSync('node ratelimit/ratelimit-testADE-Tier3.js --tier staging --rpm 100 --duration 3', { encoding: 'utf-8' });
   fs.writeFileSync('ADE-Tier3.txt', output3);
+
+  const output4 = execSync('node ratelimit/ratelimit-testAOD.js --tier staging --rpm 30 --duration 3', { encoding: 'utf-8' });
+  fs.writeFileSync('AOD.txt', output4);
+
+  const output5 = execSync('node ratelimit/ratelimit-testSam2.js --tier staging --rpm 100 --duration 3', { encoding: 'utf-8' });
+  fs.writeFileSync('Sam2.txt', output5);
+
+  const output6 = execSync('node ratelimit/ratelimit-testCountCounting.js --tier staging --rpm 100 --duration 3', { encoding: 'utf-8' });
+  fs.writeFileSync('Sam2.txt', output6);
   
 } catch (error) {
   console.error('One of the scripts failed:', error);
