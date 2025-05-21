bb0ccb84cef9dc05c642a04a70518f76f3cd0924
luisafgiraldo <144754787+luisafgiraldo@users.noreply.github.com>
2025-05-21 13:49:48 -0500

    Update runner.js

--- runner.js
+++ runner.js
@@ -2,18 +2,17 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 
 try {
-  const output1 = execSync('node ratelimit/ratelimit-testADE-Tier1.js --tier staging --rpm 25 --duration 3', { stdio: 'pipe', encoding: 'utf-8' });
-  console.log(output1); // <-- Imprime todos los logs en consola
+  const output1 = execSync('node ratelimit/ratelimit-testADE-Tier1.js --tier staging --rpm 25 --duration 3', { encoding: 'utf-8' });
   fs.writeFileSync('ADE-Tier1.txt', output1);
 
-  //const output2 = execSync('node ratelimit/ratelimit-testADE-Tier2.js --tier staging --rpm 40 --duration 3', { encoding: 'utf-8' });
-  //fs.writeFileSync('ADE-Tier2.txt', output2);
+  const output2 = execSync('node ratelimit/ratelimit-testADE-Tier2.js --tier staging --rpm 40 --duration 3', { encoding: 'utf-8' });
+  fs.writeFileSync('ADE-Tier2.txt', output2);
 
- // const output3 = execSync('node ratelimit/ratelimit-testADE-Tier3.js --tier staging --rpm 80 --duration 3', { encoding: 'utf-8' });
-  //fs.writeFileSync('ADE-Tier3.txt', output3);
+  const output3 = execSync('node ratelimit/ratelimit-testADE-Tier3.js --tier staging --rpm 80 --duration 3', { encoding: 'utf-8' });
+  fs.writeFileSync('ADE-Tier3.txt', output3);
 
-  //const output4 = execSync('node ratelimit/ratelimit-testAOD.js --tier staging --rpm 25 --duration 3', { encoding: 'utf-8' });
-  //fs.writeFileSync('AOD.txt', output4);
+  const output4 = execSync('node ratelimit/ratelimit-testAOD.js --tier staging --rpm 25 --duration 3', { encoding: 'utf-8' });
+  fs.writeFileSync('AOD.txt', output4);
 
   //const output5 = execSync('node ratelimit/ratelimit-testSam2.js --tier staging --rpm 100 --duration 3', { encoding: 'utf-8' });
   //fs.writeFileSync('Sam2.txt', output5);
