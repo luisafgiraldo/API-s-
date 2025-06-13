6b8e23ae1cb2bc65fe3362d4827092454573f2cc
DanGH1 <daniel.giraldo.external@landing.ai>
2025-06-13 18:01:37 -0500

    Test behvaior when it fails was the expected one. Re-modifying the pdf file name for the right one and checking the r before the path

--- fieldex/scripts/3-gh-id-fieldex-STG.py
+++ fieldex/scripts/3-gh-id-fieldex-STG.py
@@ -6,8 +6,8 @@ def process_documents(retry_count=2):
     headers = {"Authorization": f"Basic {VA_API_KEY}"}
     url = "https://api.va.staging.landing.ai/v1/tools/agentic-document-analysis"
 
-    base_pdf_path = "fieldex/files"
-    pdf_name = "2-PDF-id-cards.pdf"
+    base_pdf_path = r"fieldex/files"
+    pdf_name = "PDF-id-cards.pdf"
     pdf_path = f"{base_pdf_path}/{pdf_name}"
 
     schema_name = "idcards-schema-def.json"
